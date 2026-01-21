cadbf923696363f275907a66d691bf8b26e13e4e
JamesPiaggio <james.piaggio13@gmail.com>
2026-01-20 23:47:34 -0500

    Added rotate and update methods

--- player.py
+++ player.py
@@ -2,11 +2,13 @@ import pygame
 import constants
 import circleshape
 
+# Creates a Player object
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation = 0
 
+    # Method to set triangle dimensions and movement
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -15,5 +17,21 @@ class Player(circleshape.CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
+    # Method to draw player
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH) 
+
+    # Method to rotate player
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+    # Method for key bindings
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+            print(self.rotation)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            print(self.rotation)
