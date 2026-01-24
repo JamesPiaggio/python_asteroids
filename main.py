8ca5a7571837d34ffb2122a9b9816101c26a94db
JamesPiaggio <james.piaggio13@gmail.com>
2026-01-23 20:01:07 -0500

    Added Asteroid and AsteroidField to game loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants
 import player
+import asteroid
+import asteroidfield
 
 from logger import log_state
 
@@ -11,12 +13,17 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    # Groups containers
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     clock = pygame.time.Clock()
     dt = 0
     player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
+    asteroidfield.AsteroidField()
     new_player = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2) # Creates player instance
     clock.tick(60) # Sets FPS to 60 frames per second
 
@@ -26,10 +33,10 @@ def main():
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 return
-        updatable.update(dt) # Updates players direction
+        updatable.update(dt) # Triggers the update method for the updateable grouping
         screen.fill("black")
-        for drawing in drawable:
-            drawing.draw(screen) # Displays players current position
+        for drawing in drawable: # Triggers the draw method for each in drawable grouping
+            drawing.draw(screen) # Draws each object
         pygame.display.flip()
         dt = clock.tick() / 1000 # Calculates the delta time
 
