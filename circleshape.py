edea67a8730e0813d352812bdf0c01a286ee26d4
JamesPiaggio <james.piaggio13@gmail.com>
2026-01-30 16:53:21 -0500

    Added collides_with method

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,6 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -20,3 +19,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        if (self.radius + other.radius) >= self.position.distance_to(other.position):
+            return True
+        else:
+            return False
+        
