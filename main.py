f088430a0967e6a10a90d352ed342211ee7a7e18
JamesPiaggio <james.piaggio13@gmail.com>
2026-01-30 16:52:02 -0500

    Added Shot container to game loop

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import constants
 import player
 import asteroid
 import asteroidfield
+import sys
+from shot import Shot
 
-from logger import log_state
+from logger import log_state, log_event
 
 def main():
     pygame.init()
@@ -16,13 +18,15 @@ def main():
     # Groups containers
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     clock = pygame.time.Clock()
     dt = 0
     player.Player.containers = (updatable, drawable)
-    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroid.Asteroid.containers = (asteroid_group, updatable, drawable)
     asteroidfield.AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     asteroidfield.AsteroidField()
     new_player = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2) # Creates player instance
     clock.tick(60) # Sets FPS to 60 frames per second
@@ -34,6 +38,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt) # Triggers the update method for the updateable grouping
+        for a in asteroid_group:
+            if a.collides_with(new_player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
         screen.fill("black")
         for drawing in drawable: # Triggers the draw method for each in drawable grouping
             drawing.draw(screen) # Draws each object
