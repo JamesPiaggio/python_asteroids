df3f4d7c1dded3a1906f4cb5c2b9432581c09966
JamesPiaggio <james.piaggio13@gmail.com>
2026-02-03 22:30:54 -0500

    Added split method

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import LINE_WIDTH
+import constants
+from logger import log_event
+import random
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -8,8 +10,24 @@ class Asteroid(CircleShape):
 
     # Draws asteroid
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
 
     # Updates asteroid to allow for constant straight line speed
     def update(self, dt):
         self.position += self.velocity * dt
+
+    # Splits asteroid when shot
+    def split(self):
+        self.kill()
+        if self.radius <= constants.ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            vector1 = self.velocity.rotate(random_angle)
+            vector2 = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - constants.ASTEROID_MIN_RADIUS
+            first = Asteroid(self.position.x, self.position.y, new_radius)
+            second = Asteroid(self.position.x, self.position.y, new_radius)
+            first.velocity = vector1 * 1.2
+            second.velocity = vector2 * 1.2
